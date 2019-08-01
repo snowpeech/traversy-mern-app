dd3d93923a819ac302796c49458ee5b687c48470
Lydia <snowpeech@gmail.com>
2019-08-01 14:29:10 -0500

    login with token

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,11 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
+} from "../actions/types";
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -11,7 +18,16 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
+
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem("token", payload.token);
       return {
         ...state,
@@ -20,6 +36,8 @@ export default function(state = initialState, action) {
         loading: false
       };
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem("token");
       return {
         ...state,
@@ -27,6 +45,7 @@ export default function(state = initialState, action) {
         isAuthenticated: false,
         loading: false
       };
+
     default:
       return state;
   }
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,13 @@
+//function that takes in token and adds it to header, or deletes it from header
+
+import axios from "axios";
+
+const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
+
+export default setAuthToken;
