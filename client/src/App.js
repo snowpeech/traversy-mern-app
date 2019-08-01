a954bbeeb173411ee586190bcb0f91aaf46bd7d4
Lydia <snowpeech@gmail.com>
2019-07-31 20:19:21 -0500

    starting to add react components and work on front end. landing and nav bar

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+    </Fragment>
+  </Router>
+);
+
+export default App;
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -40,4 +40,78 @@ router.post('/', [auth, [check('text', 'Text is required').not().isEmpty()]],
         }
     });
 
+//@route    GET api/posts
+//@desc     get all posts
+//@access   Public? private... 
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({
+            date: -1
+        });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route    GET api/posts/:id
+//@desc     get a post by post id
+//@access   private.. (need to be logged in and have token). 
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({
+                msg: 'Post not found'
+            });
+        }
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === "ObjectId") {
+            return res.status(404).json({
+                msg: 'Post not found'
+            })
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route    delete api/posts/:id
+//@desc     delete a posts
+//@access   Private... 
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({
+                msg: 'Post not found'
+            });
+        }
+        //check user is owner of post to be deleted
+        // if (post.user.toString !== req.user.id) {
+        //     return res.status(301).json({
+        //         msg: 'User not authorized'
+        //     })
+        // }
+
+        await post.remove();
+
+        res.json({
+            msg: 'post removed'
+        });
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === "ObjectId") {
+            return res.status(404).json({
+                msg: 'Post not found'
+            })
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+
 module.exports = router;
