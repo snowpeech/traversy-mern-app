469ddc73e5712194bf18708ee250ef8c8a89b12a
Lydia <snowpeech@gmail.com>
2019-10-07 16:37:14 -0500

    adding delete user profile route

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = function (req, res, next) {
     }
 
     try {
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        const decoded = jwt.verify(token, config.get('jwtSecret')); //jwt.verify takes in token and secret as params
 
         req.user = decoded.user;
         next();
