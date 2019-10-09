a0d2e587ec6ac9ce8109a492fe7db37a5cd01cba
Lydia <snowpeech@gmail.com>
2019-10-09 15:46:12 -0500

    delete comment route complete

--- routes/api/APIposts.js
+++ routes/api/APIposts.js
@@ -165,4 +165,40 @@ router.post(
   }
 );
 
+//@route    delete comment/:id
+//@desc     delete a comment
+//@access   Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+    if (!comment) {
+      return res.status(404).json({
+        msg: "Comment not found"
+      });
+    }
+    // check user is owner of post to be deleted
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(301).json({
+        msg: "User not authorized"
+      });
+    }
+
+    const removeIndex = post.comments
+      .map(comemnt => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
